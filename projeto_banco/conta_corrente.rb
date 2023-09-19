19baf4352628735d90097ccecbdf187920159341
hetoi <luisheitor289@gmail.com>
2023-09-19 14:00:31 -0300

    escrita da classe conta corrente

--- projeto_banco/conta_corrente.rb
+++ projeto_banco/conta_corrente.rb
@@ -2,3 +2,23 @@
 # acrescentar atributo limite
 # copiar atributos e modificar método 'sacar' colocando
 # limite como parametro
+require_relative "conta.rb"
+
+class Conta_corrente
+
+    attr_accessor :limite
+
+    def initialize(nome, titular, saldo, limite)
+        super(nome, titular, saldo)
+        @limite = limite
+    end
+
+    def sacar(valor)
+        if (saldo + limite) >= valor
+            @saldo -= valor
+        else
+            puts "Operação inválida"
+        end
+    end
+
+end
